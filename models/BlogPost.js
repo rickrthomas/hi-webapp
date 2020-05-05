8310bb53cd0a209578b7694b8fcc6cac0839bb2b
Rick Thomas <slocalrick@gmail.com>
2020-05-05 16:40:29 -0700

    1.1

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,11 +4,11 @@ const mongoose = require ('mongoose');
 // Schema
 const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema ({
-    expireAt: {
-        type: Date,
-        default: Date.now,
-        index: { expires: '7d' },
-      },
+    // expireAt: {
+    //    type: Date,
+    //    default: Date.now,
+    //    index: { expires: '7d' },
+    //  },
       
     first: String,
     last: String,
